3f38fac447674b334be6b0d905e8ceacc50c8790
wannian1997 <1831106216@qq.com>
2022-05-23 22:00:08 +0800

    网站项目备份

--- src/main/java/com/smartlaw/www/SmartlawApplication.java
+++ src/main/java/com/smartlaw/www/SmartlawApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
 public class SmartlawApplication {
 
 	public static void main(String[] args) {
